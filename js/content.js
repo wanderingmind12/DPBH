a661f5ac3cfd7b8b98bd4ab0c4b82e3bfd03c1c3
Yashaswi <yashaswikhurana.bt22cseds@pec.edu.in>
2024-01-27 19:59:11 +0530

    c2

--- js/content.js
+++ js/content.js
@@ -4,3 +4,25 @@ window.addEventListener('load', function() {
     checkboxes[i].checked = false;
   }
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'checkCheckboxes') {
+    checkAllCheckboxes();
+  }
+  else if (request.action === 'uncheckCheckboxes') {
+    uncheckAllCheckboxes();
+  }
+});
+
+function checkAllCheckboxes() {
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function(checkbox) {
+    checkbox.checked = checkbox.defaultChecked;
+  });
+}
+function uncheckAllCheckboxes() {
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function(checkbox) {
+    checkbox.checked = false;
+  });
+}
---
+++ js/popup.js
@@ -0,0 +1,10 @@
+document.getElementById('checkBtn').addEventListener('click', function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'checkCheckboxes' });
+  });
+});
+document.getElementById('uncheckBtn').addEventListener('click', function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'uncheckCheckboxes' });
+  });
+});
